760faa94664315b04f9c319bd87a82a0b80ffdb2
Kishor Bibin <154350679+Kishor-Bibin@users.noreply.github.com>
2024-10-23 12:10:22 +0530

    update

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_m5PCxhAhQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 've07ns1avtqm3tvs94tvpgqs1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://idflcyd2ge.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
